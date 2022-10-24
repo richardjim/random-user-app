68fba1f39e50aacf6d76eacdacc2f54ce50ed0ff
richardjim <richardiyevhobu@gmail.com>
2022-10-24 16:01:52 +0100

    update err

--- src/views/users.jsx
+++ src/views/users.jsx
@@ -1,10 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { GrLocation, GrLocationPin } from "react-icons/gr";
 import moment from "moment";
 import { AiOutlineMail } from "react-icons/ai";
-import { FaMobileAlt } from "react-icons/fa";
-import { FcCellPhone } from "react-icons/fc";
-// import Card from "../components/card";
 import Pagination from "../components/paginate";
 
 const baseApi = `https://randomuser.me/api?results=50`;
@@ -38,19 +34,8 @@ const User = () => {
             login: { uuid, username },
             name: { title, first, last },
             gender,
-            location: {
-              street: { number, name },
-              city,
-              state,
-              country,
-              postcode,
-              coordinates: { latitude, longitude },
-              timezone: { offset, description },
-            },
             email,
             dob: { date, age },
-            phone,
-            cell,
             picture: { large },
           } = user;
 
@@ -83,40 +68,8 @@ const User = () => {
                   <small className="block mt-2 text-xl">{age} Years</small>
                   <small className="block mt-2 text-xl">@{username}</small>
                 </div>
-            
-                {/* <div className="my-10 xl:my-0">
-                  <p className="flex items-center mt-1">
-                    <FaMobileAlt className="mr-2" /> {phone}
-                  </p>
-                  <p className="flex items-center mt-1">
-                    <FcCellPhone className="mr-2" /> {cell}
-                  </p>
-                </div> */}
-
-                {/* <ul className="my-20 xl:my-0">
-                  <GrLocationPin className="text-4xl" />
-                  <li>Country: {country}</li>
-                  <li>City: {city}</li>
-                  <li>State: {state}</li>
-                  <li>Postal Code: {postcode}</li>
-                  <li>
-                    Street: {number}, {name}
-                  </li>
-                </ul> */}
-
-                {/* <div>
-                  <GrLocation className="text-4xl" />
-                  <p>Latitude: {latitude}</p>
-                  <p>Longitude: {longitude}</p>
-                </div> */}
-              </div>
-
-              {/* <div className="xl:bg-gray-800 xl:text-white xl:px-8 xl:rounded-b-lg xl:pb-8">
-                <p>
-                  Timezone: {offset}, {description}
-                </p>
-              </div> */}
-            </div>
+        </div>
+        </div>
           );
         })}
         
